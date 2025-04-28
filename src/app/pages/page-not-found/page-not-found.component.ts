2ab538472c2a83beb2042a5de5960efe61a30b0d
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-04-28 09:19:20 +0530

    Update styling

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
   templateUrl: './page-not-found.component.html',
+  imports: [
+    RouterLink
+  ]
 })
 export class PageNotFoundComponent {
 
